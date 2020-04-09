1b0185180457f5b3ab3843b04596c79d060d19d4
EnvickDohn <62852370+EnvickDohn@users.noreply.github.com>
2020-04-09 15:02:19 +0300

    Add files via upload

---
+++ precache-manifest.3fb54a3501edff9fd95094d1d23c3278.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "866bc54282f3207d9cf4a108ed810922",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94419f91ec8328a27e06",
+    "url": "/static/css/main.f462e595.chunk.css"
+  },
+  {
+    "revision": "ee944018665aa0f23d68",
+    "url": "/static/js/2.374defb0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.374defb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94419f91ec8328a27e06",
+    "url": "/static/js/main.7f4c1606.chunk.js"
+  },
+  {
+    "revision": "f8c5a9746872c0a14489",
+    "url": "/static/js/runtime-main.d6879700.js"
+  }
+]);
